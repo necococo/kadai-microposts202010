3668d1cf53fa71e86354dbe8ea3c3b5cae3631eb
necococo <moteavjobs@me.com>
2020-10-25 06:25:07 +0000

    timeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <!--もしログインしてたらそのインスタンスを獲得して名前表示-->
     @if (Auth::check())
-        <?php $user = Auth::user(); ?>
-        {{ $user->name }}
-        
+        <div class="row">
+            <aside class="col-xs-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{ $user->name }}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                    </div>
+                </div>
+            </aside>
+            <div class="col-xs-8">
+                @if (count($microposts) > 0)
+                    @include('microposts.microposts', ['microposts' => $microposts])
+                @endif
+            </div>
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Microposts2nd</h1>
+                <h1>Welcome to the Microposts</h1>
                 {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
